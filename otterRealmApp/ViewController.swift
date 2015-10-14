be4c5e901b92cfb5fa7e18af0af8def975f92001
kfagerholt <fage7951@M506-246-25987.local>
2015-10-14 15:51:43 -0700

    sidebar menu 3

--- otterRealmApp/ViewController.swift
+++ otterRealmApp/ViewController.swift
@@ -10,15 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var menuButton: UIBarButtonItem!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        
-        menuButton.target = self.revealViewController()
-        menuButton.action = Selector("revealToggle:")
-        
-        self.view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
+
     }
 
     override func didReceiveMemoryWarning() {
