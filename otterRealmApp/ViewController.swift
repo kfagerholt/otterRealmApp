21f6a02e916e9244cafdff07a0bea50d734de1fe
kfagerholt <fage7951@M506-246-25987.local>
2015-10-14 11:35:50 -0700

    sidebarmenu 1
    Added three cells in the table in menu. No connection to new views yet

--- otterRealmApp/ViewController.swift
+++ otterRealmApp/ViewController.swift
@@ -10,9 +10,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var menuButton: UIBarButtonItem!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        menuButton.target = self.revealViewController()
+        menuButton.action = Selector("revealToggle:")
+        
+        self.view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
     }
 
     override func didReceiveMemoryWarning() {
