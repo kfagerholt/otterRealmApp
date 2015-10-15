a2a3f856b232f05cc347e91dbd766a88c9d86f5e
kfagerholt <fage7951@M506-246-25987.local>
2015-10-15 14:47:18 -0700

    mainscroll in homeview

--- otterRealmApp/HomeViewController.swift
+++ otterRealmApp/HomeViewController.swift
@@ -10,20 +10,29 @@ import UIKit
 
 class HomeViewController: UIViewController {
     @IBOutlet weak var menuButton: UIBarButtonItem!
+    @IBOutlet weak var Scroller: UIScrollView!
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         if self.revealViewController() != nil {
             menuButton.target = self.revealViewController()
             menuButton.action = "revealToggle:"
             self.view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
         }
+        
+        
+        Scroller.contentSize.height = 1000
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
     
 
     /*
